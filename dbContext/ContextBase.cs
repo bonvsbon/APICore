c1bdcc4a34879c18f9be87a71e76490879e6b27c
pensmon <pensmon@hotmail.com>
2021-03-31 10:35:37 +0700

    Encrypt Connection String

--- dbContext/ContextBase.cs
+++ dbContext/ContextBase.cs
@@ -43,7 +43,8 @@ namespace APICore.dbContext
 
         public ContextBase(string ConString) : this(ConString, ConString.ToUpper().IndexOf(".MDB") > 1 || ConString.ToUpper().IndexOf(".XLS") > 1 || ConString.ToUpper().IndexOf(".CSV") > 1 ? DBType.OleDb : DBType.SqlServer)
         {
-            _SqlDb = new SqlConnection(ConString);
+            
+            _SqlDb = new SqlConnection(AESEncrypt.AESOperation.DecryptString(ConString));
         }
         public string BaseConnectionString()
         {
@@ -149,8 +150,10 @@ namespace APICore.dbContext
 
             public ResultAccess(IOptions<StateConfigs> option) : base(option.Value.ConnectionStrings._prod)
             {
+                string cons = "";
                 _dal = new ContextBase(option);
-                _dal._SqlDb = new SqlConnection(option.Value.ConnectionStrings._prod);
+                cons = AESEncrypt.AESOperation.DecryptString(option.Value.ConnectionStrings._prod);
+                _dal._SqlDb = new SqlConnection(cons);
                 dt = new DataTable();
                 responseModel = new ResponseModel();
                 _func = new Functional();
